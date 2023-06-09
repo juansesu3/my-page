14a5abf0dc7836b639c90e8e5bfd8257fee2634f
juansesu3 <juanseu3@hotmail.com>
2023-06-09 10:51:16 +0200

    working on home page and starting with about page

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,11 +1,23 @@
 import Image from "next/image";
 import styles from "./page.module.css";
-import Hero from "../../public/hero.png"
- 
+import Hero from "../../public/hero.png";
+
 const Home = () => {
   return (
     <div className={styles.container}>
-    <Image src={Hero} alt="hero-image" width={350} height={350} className={styles.img}/>
+      <div className={styles.item}>
+        <h1 className={styles.title}>
+          Better desing for your digital products
+        </h1>
+        <p className={styles.description}>
+          Turning your idea into Reality. We bring together the teams form the
+          global tech industry.
+        </p>
+        <button className={styles.button}>See Our Works</button>
+      </div>
+      <div className={styles.item}>
+        <Image src={Hero} alt="hero-image" className={styles.img} />
+      </div>
     </div>
   );
 };
