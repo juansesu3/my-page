b4f92e44dd7c32d6cb21578751109203eb59102d
juansesu3 <juanseu3@hotmail.com>
2023-06-09 17:39:22 +0200

    finished with design portafolio page

--- src/app/portfolio/[category]/page.jsx
+++ src/app/portfolio/[category]/page.jsx
@@ -1,8 +1,61 @@
+import react from "react";
+import styles from "./page.module.css";
+import Button from "@/components/button/Button";
+import Image from "next/image";
 
-const Category = () => {
+const Category = ({ params }) => {
   return (
-    <div >Category</div>
-  )
-}
+    <div className={styles.container}>
+      <h1 className={styles.catTitle}>{params.category}</h1>
+      <div className={styles.item}>
+        <div className={styles.content}>
+          <h1 className={styles.title}>Test</h1>
+          <p className={styles.desc}>Desc</p>
+          <Button text="See More" url="#" />
+        </div>
+        <div className={styles.imgConatainer}>
+          <Image
+            src="https://juan-sesu-ecommerce.s3.amazonaws.com/1685800179423.jpeg"
+            fill={true}
+            className={styles.img}
+            alt="iñustrations-image"
+          />
+        </div>
+      </div>
 
-export default Category
+      <div className={styles.item}>
+        <div className={styles.content}>
+          <h1 className={styles.title}>Test</h1>
+          <p className={styles.desc}>Desc</p>
+          <Button text="See More" url="#" />
+        </div>
+        <div className={styles.imgConatainer}>
+          <Image
+            src="https://juan-sesu-ecommerce.s3.amazonaws.com/1685800179423.jpeg"
+            fill={true}
+            className={styles.img}
+            alt="iñustrations-image"
+          />
+        </div>
+      </div>
+
+      <div className={styles.item}>
+        <div className={styles.content}>
+          <h1 className={styles.title}>Test</h1>
+          <p className={styles.desc}>Desc</p>
+          <Button text="See More" url="#" />
+        </div>
+        <div className={styles.imgConatainer}>
+          <Image
+            src="https://juan-sesu-ecommerce.s3.amazonaws.com/1685800179423.jpeg"
+            fill={true}
+            className={styles.img}
+            alt="iñustrations-image"
+          />
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Category;
