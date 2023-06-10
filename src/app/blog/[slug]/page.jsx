b328719d69a03bd269f7c95cd3aba2f3ede67150
juansesu3 <juanseu3@hotmail.com>
2023-06-10 13:34:16 +0200

    finished with blog design and I'm going to start with dark mode

--- src/app/blog/[slug]/page.jsx
+++ src/app/blog/[slug]/page.jsx
@@ -1,9 +1,59 @@
-import styles from './page.module.css';
+import Image from "next/image";
+import styles from "./page.module.css";
 
 const BlogPost = () => {
   return (
-    <div className={styles.container}>BlogPost</div>
-  )
-}
+    <div className={styles.container}>
+      <div className={styles.top}>
+        <div className={styles.info}>
+          <h1 className={styles.title}>
+            Lorem ipsum dolor sit amet consectetur, adipisicing elit. Nihil
+          </h1>
+          <p className={styles.desc}>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Iste
+            accusantium dolor, distinctio excepturi iusto atque sunt non tempora
+            deserunt earum veniam illum quod. Culpa tenetur rem dolorem facere
+            suscipit! Atque placeat enim sint magni corporis consequatur eos
+            magnam similique animi? Amet dolor optio rem vero? Ducimus numquam,
+            nihil, pariatur perferendis eum libero tempore deserunt, eos amet
+            totam in aut fugit?
+          </p>
+          <div className={styles.author}>
+            <Image
+              src=""
+              alt=""
+              width={40}
+              height={40}
+              className={styles.avatar}
+            />
+            <span className={styles.username}>Jhon Doe</span>
+          </div>
+        </div>
+        <div className={styles.imageContainer}>
+          <Image src="https://juan-sesu-ecommerce.s3.amazonaws.com/1686396482134.jpg" alt="" fill={true} className={styles.image} />
+        </div>
+      </div>
+      <div className={styles.content}>
+        <p className={styles.text}>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Soluta velit
+          eaque, omnis tempore, accusamus dolor nam hic eveniet maiores commodi
+          consectetur expedita labore vero est animi, repellendus inventore?
+          Molestias, cupiditate.<br/><br/>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Soluta velit
+          eaque, omnis tempore, accusamus dolor nam hic eveniet maiores commodi
+          consectetur expedita labore vero est animi, repellendus inventore?
+          Molestias, cupiditate.      Lorem ipsum dolor sit amet consectetur adipisicing elit. Soluta velit
+          eaque, omnis tempore, accusamus dolor nam hic eveniet maiores commodi
+          consectetur expedita labore vero est animi, repellendus inventore?
+          Molestias, cupiditate.
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Soluta velit
+          eaque, omnis tempore, accusamus dolor nam hic eveniet maiores commodi
+          consectetur expedita labore vero est animi, repellendus inventore?
+          Molestias, cupiditate.
+        </p>
+      </div>
+    </div>
+  );
+};
 
-export default BlogPost
+export default BlogPost;
--- src/app/blog/page.jsx
+++ src/app/blog/page.jsx
@@ -1,9 +1,57 @@
-import React from 'react'
-import styles from './page.module.css';
+import React from "react";
+import styles from "./page.module.css";
+import Link from "next/link";
+import Image from "next/image";
 const Blog = () => {
   return (
-    <div className={styles.container}>Blog</div>
-  )
-}
+    <div className={styles.mainContainer}>
+      <Link href="/blog/testId" className={styles.container}>
+        <div className={styles.imgContainer}>
+          <Image
+            src="https://juan-sesu-ecommerce.s3.amazonaws.com/1686076717056.jpeg"
+            alt=""
+            width={400}
+            height={250}
+            className={styles.image}
+          />
+        </div>
+        <div className={styles.content}>
+          <h1 className={styles.title}>Test</h1>
+          <p className={styles.desc}>Desc</p>
+        </div>
+      </Link>
+      <Link href="/testId" className={styles.container}>
+        <div className={styles.imgContainer}>
+          <Image
+            src="https://juan-sesu-ecommerce.s3.amazonaws.com/1686076717056.jpeg"
+            alt=""
+            width={400}
+            height={250}
+            className={styles.image}
+          />
+        </div>
+        <div className={styles.content}>
+          <h1 className={styles.title}>Test</h1>
+          <p className={styles.desc}>Desc</p>
+        </div>
+      </Link>
+      <Link href="/testId" className={styles.container}>
+        <div className={styles.imgContainer}>
+          <Image
+            src="https://juan-sesu-ecommerce.s3.amazonaws.com/1686076717056.jpeg"
+            alt=""
+            width={400}
+            height={250}
+            className={styles.image}
+          />
+        </div>
+        <div className={styles.content}>
+          <h1 className={styles.title}>Test</h1>
+          <p className={styles.desc}>Desc</p>
+        </div>
+      </Link>
+    </div>
+  );
+};
 
-export default Blog
+export default Blog;
