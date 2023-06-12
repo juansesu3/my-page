a519be7ce0694162138e8e609286c4350f453ecb
juansesu3 <juanseu3@hotmail.com>
2023-06-12 09:58:47 +0200

    I was working on fetching data from local storage now I'm going to ceate my own api to get data from there

---
+++ src/app/portfolio/[category]/data.js
@@ -0,0 +1,86 @@
+export const items = {
+  applications: [
+    {
+      id: 1,
+      title: "E-commerce Client",
+      desc: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Iste accusantium dolor, distinctio excepturi iusto atque sunt non tempora deserunt earum veniam illum quod. Culpa tenetur rem dolorem facere suscipit! Atque placeat enim sint magni corporis consequatur eos magnam similique animi? Amet dolor optio rem vero? Ducimus numquam, nihil, pariatur perferendis eum libero tempore deserunt, eos amet totam in aut fugit",
+      image: "https://juan-sesu-ecommerce.s3.amazonaws.com/1683296436645.jpg",
+    },
+    {
+      id: 2,
+      title: "E-commerce Admin",
+      desc: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Iste accusantium dolor, distinctio excepturi iusto atque sunt non tempora deserunt earum veniam illum quod. Culpa tenetur rem dolorem facere suscipit! Atque placeat enim sint magni corporis consequatur eos magnam similique animi? Amet dolor optio rem vero? Ducimus numquam, nihil, pariatur perferendis eum libero tempore deserunt, eos amet totam in aut fugit",
+      image:
+        "https://juan-sesu-ecommerce.s3.amazonaws.com/1683296447685.jpg",
+    },
+    {
+      id: 3,
+      title: "CarWash",
+      desc: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Iste accusantium dolor, distinctio excepturi iusto atque sunt non tempora deserunt earum veniam illum quod. Culpa tenetur rem dolorem facere suscipit! Atque placeat enim sint magni corporis consequatur eos magnam similique animi? Amet dolor optio rem vero? Ducimus numquam, nihil, pariatur perferendis eum libero tempore deserunt, eos amet totam in aut fugit",
+      image:
+        "https://juan-sesu-ecommerce.s3.amazonaws.com/1683726763688.jpg",
+    },
+    {
+      id: 4,
+      title: "E-commerce client",
+      desc: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Iste accusantium dolor, distinctio excepturi iusto atque sunt non tempora deserunt earum veniam illum quod. Culpa tenetur rem dolorem facere suscipit! Atque placeat enim sint magni corporis consequatur eos magnam similique animi? Amet dolor optio rem vero? Ducimus numquam, nihil, pariatur perferendis eum libero tempore deserunt, eos amet totam in aut fugit",
+      image: "https://juan-sesu-ecommerce.s3.amazonaws.com/1684859005486.webp",
+    },
+  ],
+  illustrations: [
+    {
+      id: 1,
+      title: "E-commerce Client",
+      desc: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Iste accusantium dolor, distinctio excepturi iusto atque sunt non tempora deserunt earum veniam illum quod. Culpa tenetur rem dolorem facere suscipit! Atque placeat enim sint magni corporis consequatur eos magnam similique animi? Amet dolor optio rem vero? Ducimus numquam, nihil, pariatur perferendis eum libero tempore deserunt, eos amet totam in aut fugit",
+      image: "https://juan-sesu-ecommerce.s3.amazonaws.com/1683296436645.jpg",
+    },
+    {
+      id: 2,
+      title: "E-commerce Admin",
+      desc: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Iste accusantium dolor, distinctio excepturi iusto atque sunt non tempora deserunt earum veniam illum quod. Culpa tenetur rem dolorem facere suscipit! Atque placeat enim sint magni corporis consequatur eos magnam similique animi? Amet dolor optio rem vero? Ducimus numquam, nihil, pariatur perferendis eum libero tempore deserunt, eos amet totam in aut fugit",
+      image:
+        "https://juan-sesu-ecommerce.s3.amazonaws.com/1683296447685.jpg",
+    },
+    {
+      id: 3,
+      title: "CarWash",
+      desc: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Iste accusantium dolor, distinctio excepturi iusto atque sunt non tempora deserunt earum veniam illum quod. Culpa tenetur rem dolorem facere suscipit! Atque placeat enim sint magni corporis consequatur eos magnam similique animi? Amet dolor optio rem vero? Ducimus numquam, nihil, pariatur perferendis eum libero tempore deserunt, eos amet totam in aut fugit",
+      image:
+        "https://juan-sesu-ecommerce.s3.amazonaws.com/1683726763688.jpg",
+    },
+    {
+      id: 4,
+      title: "E-commerce client",
+      desc: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Iste accusantium dolor, distinctio excepturi iusto atque sunt non tempora deserunt earum veniam illum quod. Culpa tenetur rem dolorem facere suscipit! Atque placeat enim sint magni corporis consequatur eos magnam similique animi? Amet dolor optio rem vero? Ducimus numquam, nihil, pariatur perferendis eum libero tempore deserunt, eos amet totam in aut fugit",
+      image: "https://juan-sesu-ecommerce.s3.amazonaws.com/1684859005486.webp",
+    },
+  ],
+  websites: [
+    {
+      id: 1,
+      title: "E-commerce Client",
+      desc: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Iste accusantium dolor, distinctio excepturi iusto atque sunt non tempora deserunt earum veniam illum quod. Culpa tenetur rem dolorem facere suscipit! Atque placeat enim sint magni corporis consequatur eos magnam similique animi? Amet dolor optio rem vero? Ducimus numquam, nihil, pariatur perferendis eum libero tempore deserunt, eos amet totam in aut fugit",
+      image: "https://juan-sesu-ecommerce.s3.amazonaws.com/1683296436645.jpg",
+    },
+    {
+      id: 2,
+      title: "E-commerce Admin",
+      desc: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Iste accusantium dolor, distinctio excepturi iusto atque sunt non tempora deserunt earum veniam illum quod. Culpa tenetur rem dolorem facere suscipit! Atque placeat enim sint magni corporis consequatur eos magnam similique animi? Amet dolor optio rem vero? Ducimus numquam, nihil, pariatur perferendis eum libero tempore deserunt, eos amet totam in aut fugit",
+      image:
+        "https://juan-sesu-ecommerce.s3.amazonaws.com/1683296447685.jpg",
+    },
+    {
+      id: 3,
+      title: "CarWash",
+      desc: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Iste accusantium dolor, distinctio excepturi iusto atque sunt non tempora deserunt earum veniam illum quod. Culpa tenetur rem dolorem facere suscipit! Atque placeat enim sint magni corporis consequatur eos magnam similique animi? Amet dolor optio rem vero? Ducimus numquam, nihil, pariatur perferendis eum libero tempore deserunt, eos amet totam in aut fugit",
+      image:
+        "https://juan-sesu-ecommerce.s3.amazonaws.com/1683726763688.jpg",
+    },
+    {
+      id: 4,
+      title: "E-commerce client",
+      desc: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Iste accusantium dolor, distinctio excepturi iusto atque sunt non tempora deserunt earum veniam illum quod. Culpa tenetur rem dolorem facere suscipit! Atque placeat enim sint magni corporis consequatur eos magnam similique animi? Amet dolor optio rem vero? Ducimus numquam, nihil, pariatur perferendis eum libero tempore deserunt, eos amet totam in aut fugit",
+      image: "https://juan-sesu-ecommerce.s3.amazonaws.com/1684859005486.webp",
+    },
+  ],
+};
--- src/app/portfolio/[category]/page.jsx
+++ src/app/portfolio/[category]/page.jsx
@@ -2,58 +2,42 @@ import react from "react";
 import styles from "./page.module.css";
 import Button from "@/components/button/Button";
 import Image from "next/image";
+import { items } from "./data.js";
+import { notFound } from "next/navigation";
+
+console.log({items})
+
+const getData = (cat) => {
+  const data = items[cat];
+  if (data) {
+    return data;
+  }
+  return notFound();
+};
 
 const Category = ({ params }) => {
+  const data = getData(params.category);
+
   return (
     <div className={styles.container}>
       <h1 className={styles.catTitle}>{params.category}</h1>
-      <div className={styles.item}>
-        <div className={styles.content}>
-          <h1 className={styles.title}>Test</h1>
-          <p className={styles.desc}>Desc</p>
-          <Button text="See More" url="#" />
+      {data.map((item) => (
+        <div key={item.id} className={styles.item}>
+          <div className={styles.content}>
+            <h1 className={styles.title}>{item.title}</h1>
+            <p className={styles.desc}>{item.desc}</p>
+            <Button text="See More" url="#" />
+          </div>
+          <div className={styles.imgConatainer}>
+            <Image
+              src={item.image}
+              fill={true}
+              className={styles.img}
+              alt="iñustrations-image"
+            />
+          </div>
         </div>
-        <div className={styles.imgConatainer}>
-          <Image
-            src="https://juan-sesu-ecommerce.s3.amazonaws.com/1685800179423.jpeg"
-            fill={true}
-            className={styles.img}
-            alt="iñustrations-image"
-          />
-        </div>
-      </div>
-
-      <div className={styles.item}>
-        <div className={styles.content}>
-          <h1 className={styles.title}>Test</h1>
-          <p className={styles.desc}>Desc</p>
-          <Button text="See More" url="#" />
-        </div>
-        <div className={styles.imgConatainer}>
-          <Image
-            src="https://juan-sesu-ecommerce.s3.amazonaws.com/1685800179423.jpeg"
-            fill={true}
-            className={styles.img}
-            alt="iñustrations-image"
-          />
-        </div>
-      </div>
-
-      <div className={styles.item}>
-        <div className={styles.content}>
-          <h1 className={styles.title}>Test</h1>
-          <p className={styles.desc}>Desc</p>
-          <Button text="See More" url="#" />
-        </div>
-        <div className={styles.imgConatainer}>
-          <Image
-            src="https://juan-sesu-ecommerce.s3.amazonaws.com/1685800179423.jpeg"
-            fill={true}
-            className={styles.img}
-            alt="iñustrations-image"
-          />
-        </div>
-      </div>
+      ))}
     </div>
   );
 };
