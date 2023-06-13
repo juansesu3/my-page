5d66a824e919f6c3f2a408d55ae8895d59e9e405
juansesu3 <juanseu3@hotmail.com>
2023-06-13 16:48:30 +0200

    I still working on responsive webdsign

--- src/app/contact/page.jsx
+++ src/app/contact/page.jsx
@@ -1,4 +1,5 @@
-import React from "react";
+"use client";
+import React, { useEffect, useState } from "react";
 import styles from "./page.module.css";
 import Image from "next/image";
 import ImgContact from "../../../public/contact.png";
@@ -8,18 +9,38 @@ export const metadata = {
   description: "This is the contact information",
 };
 const Contacto = () => {
+  const [width, setWidth] = useState(null);
+
+  useEffect(() => {
+    // Función para manejar el evento de cambio de tamaño de la ventana
+    const handleResize = () => {
+      setWidth(window.innerWidth);
+    };
+    // Agregar el evento de cambio de tamaño de la ventana
+    window.addEventListener("resize", handleResize);
+    // Obtener la medida de la ventana al cargar el componente
+    setWidth(window.innerWidth);
+    // Eliminar el evento de cambio de tamaño de la ventana al desmontar el componente
+    return () => {
+      window.removeEventListener("resize", handleResize);
+    };
+  }, []);
+
   return (
     <div className={styles.container}>
       <h1 className={styles.title}>Let&apos;s Keep in Touch</h1>
       <div className={styles.content}>
-        <div className={styles.imgContainer}>
-          <Image
-            src={ImgContact}
-            alt="image-contact"
-            fill={true}
-            className={styles.image}
-          />
-        </div>
+      {width > 768 && (
+          <div className={styles.imgContainer}>
+            <Image
+              src={ImgContact}
+              alt="image-contact"
+              fill={true}
+              className={styles.image}
+            />
+          </div>
+        )}
+
         <form className={styles.form}>
           <input type="text" placeholder="name" className={styles.input} />
           <input type="text" placeholder="email" className={styles.input} />
@@ -31,6 +52,8 @@ const Contacto = () => {
           ></textarea>
           <Button url="#" text="Send" />
         </form>
+       
+
       </div>
     </div>
   );
